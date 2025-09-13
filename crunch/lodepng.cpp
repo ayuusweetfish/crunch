cfb7834d793be9d797fc5380131e4d86ab99573f
Ayu <claire@ayu.land>
2025-09-13 14:42:07 +0800

    Fix compilation errors & warnings (G++ 14.2.0)

--- crunch/lodepng.cpp
+++ crunch/lodepng.cpp
@@ -4473,8 +4473,8 @@ static unsigned readChunk_iTXt(LodePNGInfo* info, const LodePNGDecompressSetting
     {
       if(!ucvector_resize(&decoded, length + 1)) CERROR_BREAK(error, 83 /*alloc fail*/);
 
-      decoded.data[length] = 0;
       for(i = 0; i != length; ++i) decoded.data[i] = data[begin + i];
+      decoded.data[length] = 0;
     }
 
     error = lodepng_add_itext(info, key, langtag, transkey, (char*)decoded.data);
